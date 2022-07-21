c53e2d717ba673f2767efb31b24889d456e81af5
isabel9422 <isabels94@hotmail.com>
2022-07-21 11:02:02 +0200

    Validators uploaded

--- app/Controllers/Http/UsuariosController.ts
+++ app/Controllers/Http/UsuariosController.ts
@@ -1,3 +1,20 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Usuario from 'App/Models/Usuario'
 
-export default class UsuariosController {}
+export default class UsuariosController {
+  public async login({ request, auth, response }: HttpContextContract) {
+    const { email, password } = request.all()
+    try {
+      const token = await auth.attempt(email, password)
+      return { token: token, data: auth.user }
+    } catch (error) {
+      return response.badRequest({ error: error.message })
+    }
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const validateData = await request.validate(CrearUsuarioValidator)
+    const usuario = await Usuario.create(validateData)
+    return response.created({ data: usuario })
+  }
+}
