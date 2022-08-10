2b63780fe25752588633e0b2a89809421b475cc6
isabel9422 <isabels94@hotmail.com>
2022-08-10 21:09:25 +0200

    fixed

--- start/routes.ts
+++ start/routes.ts
@@ -1,13 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-/*Route.group(() => {
+Route.group(() => {
   Route.resource('/users', 'UsuariosController').apiOnly()
   Route.post('/log', 'UsuariosController.login')
   Route.resource('/categories', 'CategoriasController').apiOnly()
 }).prefix('/api')
-*/
-
-Route.get('/cat', 'CategoriasController.index')
 
 Route.group(() => {
   Route.resource('/posts', 'ArticulosController').apiOnly()
